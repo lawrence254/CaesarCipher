83898e7ad57f25ba59a622a4c113f874422fd3c3
komen225 <komen.robert19@gmail.com>
2019-09-01 19:04:56 +0300

    Add on the class, creation of getter and setter method and if it is a string .

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,3 +1,19 @@
 public class CaesarCipher {
 
+
+
+
+    public String getText() {
+        return text;
+
+    }
+
+   private String text;
+    private Integer key;
+
+    public CaesarCipher(String text,Integer key){
+        this.key = key;
+        this.text = text;
+    }
+
 }
--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -1,5 +1,12 @@
 import static org.junit.Assert.*;
 
+import org.junit.*;
+
 public class CaesarCipherTest {
+    @Test
+    public void text_textIsString(){
+        CaesarCipher testCaesarCipher = new CaesarCipher("a",1);
+        assertEquals(true, testCaesarCipher.getText() instanceof String);
+    }
 
 }
